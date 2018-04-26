c0c7f3efda9d742dcdcfdab0714e0e964dd6ff5e
Alexey <alexey.tuhtiaev@gmail.com>
2018-04-26 21:09:00 +0300

    get all folders

--- WpfMoreComplexApp/MainWindow.xaml.cs
+++ WpfMoreComplexApp/MainWindow.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfMoreComplexApp
 {
@@ -41,10 +31,88 @@ namespace WpfMoreComplexApp
         {
             foreach (var drive in Directory.GetLogicalDrives())
             {
-                var item = new TreeViewItem();
-                item.Header = drive;
+                var item = new TreeViewItem()
+                {
+                    Header = drive,
+                    Tag = drive
+                };
+
+
+                item.Items.Add(null);
+
+                //Listen out item being expanded
+                item.Expanded += Folder_Expanded;
+
                 FolderView.Items.Add(item);
             }
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+
+            if (item.Items.Count != 1 && item.Items[0] != null)
+            {
+                return;
+            }
+
+            item.Items.Clear();
+
+            var fullPath = (string)item.Tag;
+
+            var directories = new List<string>();
+
+            try
+            {
+                var dirs = Directory.GetDirectories(fullPath);
+                if (dirs.Length > 0)
+                {
+                    directories.AddRange(dirs);
+                }
+            }
+            catch (Exception)
+            {
+                //TODO: catching!!!!
+            }
+
+            directories.ForEach(directoryPath =>
+            {
+                var subItem = new TreeViewItem()
+                {
+                    Header = GetFileFolderName(directoryPath),
+                    Tag = directoryPath
+                };
+
+                subItem.Items.Add(null);
+                subItem.Expanded += Folder_Expanded;
+
+                item.Items.Add(subItem);
+            });
+
+        }
+
+        /// <summary>
+        /// Find file or folder name from a full path
+        /// </summary>
+        /// <param name="path">The full path</param>
+        /// <returns></returns>
+        public static string GetFileFolderName(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                return string.Empty;
+            }
+
+            var normolizePath = path.Replace('/', '\\');
+
+            var lastIndex = normolizePath.LastIndexOf('\\');
+
+            if (lastIndex <= 0)
+            {
+                return path;
+            }
+
+            return path.Substring(lastIndex+1);
         }
         #endregion
     }
